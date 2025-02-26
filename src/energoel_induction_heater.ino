c5eee54f8710d4fde194fffda7f22529b35e05b7
Filip Cichowski <filip.cichowski.dev@gmail.com>
2025-02-26 17:23:42 +0100

    Working power up/down (no electrical signal yet),working timer change,  mode switch (information is stored inside of variable, but not used)

--- src/energoel_induction_heater.ino
+++ src/energoel_induction_heater.ino
@@ -6,12 +6,16 @@
 #define SHIFT_LATCH 9
 
 #define BUTTON_MODE A4
-#define BUTTON_UP A5
-#define BUTTON_DOWN 13
+#define POWER_UP A5
+#define POWER_DOWN 13
 #define MANUAL_LED 6
 #define TIMER_LED 4
 #define EEPROM_ADDR_MODE 0
 #define EEPROM_ADDR_POWER 1
+
+#define TIME_UP 12
+#define TIME_DOWN 11
+#define EEPROM_ADDR_TIME 2
 
 volatile uint8_t currentDigit = 0;
 bool manualMode;
@@ -19,28 +23,24 @@ uint8_t powerLevel;
 bool lastButtonModeState = HIGH;
 bool modeChangeRequested = false;
 
-// Przechowywanie czasu trzymania przycisków mocy
 unsigned long buttonUpHoldStart = 0;
 unsigned long buttonDownHoldStart = 0;
 bool buttonUpHeld = false;
 bool buttonDownHeld = false;
 
-// Mapa segmentów: g, f, e, d, c, b, a (LSB = 0)
+unsigned long buttonTimeUpHoldStart = 0;
+unsigned long buttonTimeDownHoldStart = 0;
+bool buttonTimeUpHeld = false;
+bool buttonTimeDownHeld = false;
+uint16_t timerValue;
+
 const uint8_t digitMap[10] = {
-  0b01111110, // 0
-  0b00001100, // 1
-  0b10110110, // 2
-  0b10011110, // 3
-  0b11001100, // 4
-  0b11011010, // 5
-  0b11111010, // 6
-  0b00001110, // 7
-  0b11111110, // 8
-  0b11011110  // 9
+  0b01111110, 0b00001100, 0b10110110, 0b10011110,
+  0b11001100, 0b11011010, 0b11111010, 0b00001110,
+  0b11111110, 0b11011110
 };
 
-// Bufor cyfr do wyświetlacza
-uint8_t digitBuffer[6] = {0, 0, 0, 0, 0, 0};
+uint8_t digitBuffer[6];
 
 void setup() {
   pinMode(SHIFT_DATA, OUTPUT);
@@ -48,12 +48,13 @@ void setup() {
   pinMode(SHIFT_LATCH, OUTPUT);
   
   pinMode(BUTTON_MODE, INPUT_PULLUP);
-  pinMode(BUTTON_UP, INPUT_PULLUP);
-  pinMode(BUTTON_DOWN, INPUT_PULLUP);
+  pinMode(POWER_UP, INPUT_PULLUP);
+  pinMode(POWER_DOWN, INPUT_PULLUP);
   pinMode(MANUAL_LED, OUTPUT);
   pinMode(TIMER_LED, OUTPUT);
+  pinMode(TIME_UP, INPUT_PULLUP);
+  pinMode(TIME_DOWN, INPUT_PULLUP);
 
-  // Odczytaj zapisane wartości z EEPROM
   uint8_t savedMode = EEPROM.read(EEPROM_ADDR_MODE);
   manualMode = (savedMode == 0xFF) ? true : savedMode;
   EEPROM.update(EEPROM_ADDR_MODE, manualMode);
@@ -62,17 +63,20 @@ void setup() {
   powerLevel = (savedPower > 100) ? 50 : savedPower;
   EEPROM.update(EEPROM_ADDR_POWER, powerLevel);
 
+  uint16_t savedTime = EEPROM.read(EEPROM_ADDR_TIME);
+  timerValue = (savedTime > 999) ? 100 : savedTime;
+  EEPROM.update(EEPROM_ADDR_TIME, timerValue);
+
   updateLEDs();
   updatePowerDisplay();
+  updateTimerDisplay();
 
-  // Timer do multipleksowania co 2ms
   TCCR1A = 0;
   TCCR1B = (1 << WGM12) | (1 << CS11);
   OCR1A = 1999;
   TIMSK1 |= (1 << OCIE1A);
 }
 
-// ISR do multipleksowania wyświetlacza
 ISR(TIMER1_COMPA_vect) {
   digitalWrite(SHIFT_LATCH, LOW);
   shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, (1 << (6 - currentDigit)));
@@ -80,7 +84,6 @@ ISR(TIMER1_COMPA_vect) {
   digitalWrite(SHIFT_LATCH, HIGH);
   currentDigit = (currentDigit + 1) % 6;
 
-  // Obsługa zmiany trybu w przerwaniu
   static bool lastModeState = HIGH;
   bool modeState = digitalRead(BUTTON_MODE);
   
@@ -88,6 +91,79 @@ ISR(TIMER1_COMPA_vect) {
     modeChangeRequested = true;
   }
   lastModeState = modeState;
+}
+
+void handleButtonPress(bool* held, unsigned long* holdStart, unsigned long now, void (*changeValue)(int), int change) {
+  static unsigned long lastButtonPress = 0;
+
+  if (!*held) {
+    *holdStart = now;
+    *held = true;
+    changeValue(change);
+    lastButtonPress = now;
+  }
+
+  unsigned long holdTime = now - *holdStart;
+  unsigned long interval = (holdTime >= 2000) ? 100 : (holdTime >= 400) ? 400 : 1000;
+
+  if (now - lastButtonPress > interval) {
+    changeValue(change);
+    lastButtonPress = now;
+  }
+}
+
+
+void checkButtons(int buttonUp, int buttonDown, bool* upHeld, bool* downHeld, unsigned long* upHoldStart, unsigned long* downHoldStart, void (*changeValue)(int), int minValue, int maxValue) {
+  unsigned long now = millis();
+
+  if (digitalRead(buttonUp) == LOW && !*downHeld) {
+    handleButtonPress(upHeld, upHoldStart, now, changeValue, 1);
+  } else {
+    *upHeld = false;
+  }
+
+  if (digitalRead(buttonDown) == LOW && !*upHeld) {
+    handleButtonPress(downHeld, downHoldStart, now, changeValue, -1);
+  } else {
+    *downHeld = false;
+  }
+}
+
+
+
+void changePowerLevel(int change) {
+  if ((change == 1 && powerLevel < 100) || (change == -1 && powerLevel > 0)) {
+    powerLevel += change;
+    powerLevel = constrain(powerLevel, 0, 100);
+    EEPROM.update(EEPROM_ADDR_POWER, powerLevel);
+    updatePowerDisplay();
+  }
+}
+
+void changeTimerValue(int change) {
+  if ((change == 1 && timerValue < 999) || (change == -1 && timerValue > 0)) {
+    timerValue += change;
+    timerValue = constrain(timerValue, 0, 999);
+    EEPROM.update(EEPROM_ADDR_TIME, timerValue);
+    updateTimerDisplay();
+  }
+}
+
+void updateLEDs() {
+  digitalWrite(MANUAL_LED, manualMode);
+  digitalWrite(TIMER_LED, !manualMode);
+}
+
+void updatePowerDisplay() {
+  digitBuffer[3] = powerLevel / 100;
+  digitBuffer[4] = (powerLevel / 10) % 10;
+  digitBuffer[5] = powerLevel % 10;
+}
+
+void updateTimerDisplay() {
+  digitBuffer[0] = timerValue / 100;
+  digitBuffer[1] = (timerValue / 10) % 10;
+  digitBuffer[2] = timerValue % 10;
 }
 
 void loop() {
@@ -98,59 +174,6 @@ void loop() {
     updateLEDs();
   }
   
-  checkPowerButtons();
-}
-
-// **Obsługa przycisków zmiany mocy**
-void checkPowerButtons() {
-  handlePowerButton(BUTTON_UP, &buttonUpHeld, &buttonUpHoldStart, 1);
-  handlePowerButton(BUTTON_DOWN, &buttonDownHeld, &buttonDownHoldStart, -1);
-}
-
-void handlePowerButton(int button, bool* held, unsigned long* holdStart, int change) {
-  static unsigned long lastButtonPress = 0;
-  bool reading = digitalRead(button);
-  unsigned long now = millis();
-
-  if (reading == LOW) {
-    if (!*held) {
-      *holdStart = now;
-      *held = true;
-      changePowerLevel(change); // Zmieniamy wartość od razu
-      lastButtonPress = now;
-    }
-
-    unsigned long holdTime = now - *holdStart;
-    unsigned long interval = (holdTime >= 3000) ? 200 : (holdTime >= 1500) ? 400 : 1000;
-
-    if (now - lastButtonPress > interval) {
-      changePowerLevel(change);
-      lastButtonPress = now;
-    }
-  } else {
-    *held = false;
-  }
-}
-
-// **Zmiana wartości mocy**
-void changePowerLevel(int change) {
-  if ((change == 1 && powerLevel < 100) || (change == -1 && powerLevel > 0)) {
-    powerLevel += change;
-    powerLevel = constrain(powerLevel, 0, 100);
-    EEPROM.update(EEPROM_ADDR_POWER, powerLevel);
-    updatePowerDisplay();
-  }
-}
-
-// **Aktualizacja LEDów trybu**
-void updateLEDs() {
-  digitalWrite(MANUAL_LED, manualMode);
-  digitalWrite(TIMER_LED, !manualMode);
-}
-
-// **Wyświetlanie wartości mocy**
-void updatePowerDisplay() {
-  digitBuffer[3] = powerLevel / 100; // Setki
-  digitBuffer[4] = (powerLevel / 10) % 10; // Dziesiątki
-  digitBuffer[5] = powerLevel % 10; // Jedności
+  checkButtons(POWER_UP, POWER_DOWN, &buttonUpHeld, &buttonDownHeld, &buttonUpHoldStart, &buttonDownHoldStart, changePowerLevel, 0, 100);
+  checkButtons(TIME_UP, TIME_DOWN, &buttonTimeUpHeld, &buttonTimeDownHeld, &buttonTimeUpHoldStart, &buttonTimeDownHoldStart, changeTimerValue, 0, 999);
 }
